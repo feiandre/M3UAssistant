71e67ab20f3d181c8affecae102c875f91c536f8
Alan32Liu <alan32.liu@gmail.com>
2017-11-02 11:38:45 +1100

    Some colors to give prettier console output

---
+++ bcolors.py
@@ -0,0 +1,11 @@
+# Colors to be used in stdout
+
+
+class BColors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END_COLOR = '\033[0m'
+
